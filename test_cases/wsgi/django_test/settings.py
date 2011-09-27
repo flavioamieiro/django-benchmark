10b62f68bb7e7b0ea10926fb95fd82dc0ff50501
Flavio Amieiro <amieiro.flavio@gmail.com>
2011-09-27 20:48:09 -0300

    Adiciona caso em que o django busca uma resposta no disco

--- test_cases/wsgi/django_test/settings.py
+++ test_cases/wsgi/django_test/settings.py
@@ -1,3 +1,4 @@
+import os
 # Django settings for django_test project.
 
 DEBUG = True
@@ -106,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates'),
 )
 
 INSTALLED_APPS = (
--- test_cases/wsgi/django_test/urls.py
+++ test_cases/wsgi/django_test/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from direct_response.views import direct_response
+from response_from_disk.views import response_from_disk
 
 urlpatterns = patterns('^/wsgi/django_test/',
     url('direct_response/$', direct_response),
+    url('response_from_disk/$', response_from_disk),
 )
